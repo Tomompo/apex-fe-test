0fe1ece6866ee20138298c1d6ddf9f4dbad4778a
TomBickle <tom.bickle@rdcaviation.com>
2022-07-11 16:21:14 +0100

    - add new component for tightly coupled drill down

--- src/app/views/dt-drill-down-coupled-ii/dt-drill-down-coupled-ii.component.ts
+++ src/app/views/dt-drill-down-coupled-ii/dt-drill-down-coupled-ii.component.ts
@@ -24,7 +24,7 @@ export class DtDrillDownCoupledIiComponent implements OnInit {
 
   chart: any = null;
 
-  chartType: string = 'column';
+  chartType: string = 'treemap';
 
   ngxColumns: any[] = [
     { name: 'id', prop: 'id' },
