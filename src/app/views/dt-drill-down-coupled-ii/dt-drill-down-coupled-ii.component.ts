eb25992aaffeae7dc042c96a8cb49afca70907e9
TomBickle <tom.bickle@rdcaviation.com>
2022-07-11 10:56:25 +0100

    - add new component for tightly coupled drill down

---
+++ src/app/views/dt-drill-down-coupled-ii/dt-drill-down-coupled-ii.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dt-drill-down-coupled-ii',
+  templateUrl: './dt-drill-down-coupled-ii.component.html',
+  styleUrls: ['./dt-drill-down-coupled-ii.component.scss']
+})
+export class DtDrillDownCoupledIiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
