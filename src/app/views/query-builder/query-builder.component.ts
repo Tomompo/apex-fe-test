e9f207be961c0ede2cc092c088a6b9f545cf756a
TomBickle <tom.bickle@rdcaviation.com>
2022-10-14 10:42:48 +0100

    - add new page for api examples

--- src/app/views/query-builder/query-builder.component.ts
+++ src/app/views/query-builder/query-builder.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Form, FormArray, FormControl, FormGroup} from "@angular/forms";
+import {Form, FormArray, UntypedFormControl, UntypedFormGroup} from "@angular/forms";
 import {dataPointFilter} from "./data-point-filter";
 
 @Component({
@@ -11,15 +11,15 @@ export class QueryBuilderComponent implements OnInit {
 
   dpf = dataPointFilter;
 
-  form: FormGroup = new FormGroup({});
+  form: UntypedFormGroup = new UntypedFormGroup({});
 
   constructor() { }
 
   ngOnInit(): void {
 
-    this.form.addControl('data-points', new FormGroup({}));
+    this.form.addControl('data-points', new UntypedFormGroup({}));
 
-    const dataPoints = this.form.get('data-points') as FormGroup;
+    const dataPoints = this.form.get('data-points') as UntypedFormGroup;
 
     Object.keys(this.dpf["data-points"])
       .forEach(key => {
@@ -29,10 +29,10 @@ export class QueryBuilderComponent implements OnInit {
 
         // @ts-ignore
         const controls = this.dpf["data-points"][key].map((options: {id: string, label: string}) =>
-          dataPoints.addControl(options.label, new FormControl(false))
+          dataPoints.addControl(options.label, new UntypedFormControl(false))
         );
 
-        dataPoints.addControl(key, new FormGroup({}));
+        dataPoints.addControl(key, new UntypedFormGroup({}));
 
       });
 
